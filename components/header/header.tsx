c852ff3aa8292ac66c8b541dd69db460870b0b91
rehanulhb <rehanulh@gmail.com>
2024-01-12 10:53:32 +0100

    Installed DaisyUI and Setuped the Platform

---
+++ components/header/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return 
+        <header>
+            <nav className='navbar justify-between bg-base-300'>
+                <link href="/" className='btn btn-ghost text-lg'>
+                    Ecommerce
+                </link>
+            </nav>
+
+        </header>
+  
+        
+    
+  
+}
+
+export default Header
