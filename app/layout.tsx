4f34c844fe00a22471d428bf12fb870d5cb21414
rehanulhb <rehanulh@gmail.com>
2024-01-12 11:39:07 +0100

    Solved Rfce Issue with header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header/header'
 
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 
-const Header = () => {
-  return 
-        <header>
-            <nav className='navbar justify-between bg-base-300'>
-                <link href="/" className='btn btn-ghost text-lg'>
-                    Ecommerce
-                </link>
-            </nav>
-
-        </header>
-  
-        
-    
-  
+const header = () => {
+  return (
+    <header>
+        <nav className='navbar justify-between bg-base-300'></nav>
+    </header>
+  )
 }
 
-export default Header
+export default header
