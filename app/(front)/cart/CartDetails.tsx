273de8a83b56024428585601d314dbefb8bf6d18
rehanulhb <rehanulh@gmail.com>
2024-01-22 22:19:17 +0100

    Working on Cart Details Page

--- app/(front)/cart/CartDetails.tsx
+++ app/(front)/cart/CartDetails.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import useCartService from "@/lib/hooks/useCartStore"
+import Image from "next/image"
+import Link from "next/link"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
@@ -15,9 +17,54 @@ export default function CartDetails(){
 
     if(!mounted) return <></>
 
-    return 
+    return (
     <>
         <h1 className="py-4 text-2xl">Shopping Cart</h1>
+
+        {items.length === 0 ? (
+            <div>
+                Cart is empty. <Link href="/">Go Shopping</Link>
+            </div>
+            ) : (
+            <div className="grid md:grid-cols-4 md:gap-5">
+                <div className="overflow-x-auto md:col-span-3">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {items.map((item)=> (
+                                <tr key={item.slug}>
+                                    <td>
+                                        <Link href={`/product/${item.slug}`} className="flex items-center">
+                                            <Image 
+                                            src={item.image}
+                                            alt={item.name}
+                                            width={50}
+                                            height={50}>
+
+                                            </Image>
+                                            <span className="px-2">
+                                                {item.name}
+                                            </span>
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <button className="btn" type="button" onClick={()=> decrease(item)}> - </button>
+                                        <span className="px-2">{item.qty}</span>
+                                        <button className="btn" type="button" onClick={()=> increase(item)}>+</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            )}
     </>
-    
-}
+    )
+} 
