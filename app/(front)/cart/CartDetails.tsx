b2d6e0e24280700cc3d8a53f79b1ec5a800f8dbe
rehanulhb <rehanulh@gmail.com>
2024-01-22 21:58:42 +0100

    Added Function for Shopping cart

--- app/(front)/cart/CartDetails.tsx
+++ app/(front)/cart/CartDetails.tsx
@@ -2,10 +2,22 @@
 
 import useCartService from "@/lib/hooks/useCartStore"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
 export default function CartDetails(){
     const router = useRouter()
     const{items, itemsPrice, decrease, increase } = useCartService()
 
+    const [mounted,setMounted] = useState(false)
+    useEffect(()=> {
+        setMounted(true)
+    }, [])
+
+    if(!mounted) return <></>
+
+    return 
+    <>
+        <h1 className="py-4 text-2xl">Shopping Cart</h1>
+    </>
     
 }
