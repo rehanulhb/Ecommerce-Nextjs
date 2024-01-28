d5c4ce103f04bbc2faabc578280e0f41e05bc3d6
rehanulhb <rehanulh@gmail.com>
2024-01-28 02:21:53 +0100

    Created route.ts in api/products/seed folder inside of app folder and write code for Database connect

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,14 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+    users:[
+        {
+            name: 'John',
+            email: 'admin@gmail.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+    ],
     products:[
         {
             name:'Free Shirt',
--- lib/models/UserModel.ts
+++ lib/models/UserModel.ts
@@ -21,3 +21,7 @@ const UserSchema = new mongoose.Schema(
     },
     { timestamps: true }
 )
+
+const UserModel = mongoose.models.User || mongoose.model('User', UserSchema)
+
+export default UserModel
